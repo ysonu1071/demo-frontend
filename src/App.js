f89a68a63487b1c734a459ad8fbc7505d7b9bda3
ysonu1071@gmail.com <ysonu1071@gmail.com>
2024-09-15 22:22:39 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [userData, setUserData] = useState(null)
+
+  const handleSubmit = async() =>{
+    // console.log(email,'this is email')
+    fetch("http://localhost:8000/user", {
+      method: "POST",
+      headers:{
+          "Content-Type":"application/json",
+          // "Authorization":"Bearer " + token
+      },
+      body: JSON.stringify({email:email})
+      
+    })
+    .then((res)=>{
+      console.log(res, 'this is res')
+      getData();
+    })
+    .catch((err)=> {
+      console.log(err, 'err')
+    })
+  }
+
+  const getData = async() =>{
+    fetch("http://localhost:8000/")
+    .then((res)=>{
+      return res.json();
+    })
+    .then((res)=>{
+      console.log(res, 'thsi is res')
+      setUserData(res.data)
+    })
+    .catch((err)=>{
+      console.log(err, 'err')
+    })
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Welcom, this is demo frontend</h1>
+
+      <div>
+        <p>Enter email: </p>
+        <input onChange={(e)=> setEmail(e.target.value)}/>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>submit</button>
+      </div>
+      <div>
+        {userData && userData.map((user, index)=> <h3 key={index}>{user.email}</h3>)}
+      </div>
     </div>
   );
 }
